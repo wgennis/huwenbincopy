3b1cdec266bdb1d389e2cdad523f402d2cfba6a3
wgennis <91460230+wgennis@users.noreply.github.com>
2021-10-09 21:55:32 +0800

    Create main.c

---
+++ week2/ch02/main.c
@@ -0,0 +1,90 @@
+#include<math.h>
+#include<stdlib.h>
+#ifndef _AvlTree_H
+struct AvlNode;
+typedef struct AvlNode *Position;
+typedef struct AvlNode *AvlTree;
+AvlTree Insert(int X,AvlTree T);
+AvlTree Delete(int X,AvlTree T);
+#endif
+struct AvlNode
+{
+    int Data;
+    AvlTree Left;
+    AvlTree Right;
+    int Height;
+};
+static int Height(Position P)
+{
+    if(P==NULL)
+    return -1;
+    else
+    return P->Height;
+}
+static Position SingleRotateWithLeft(Position K2)
+{
+    Position K1;
+    K1=K2->Left;
+    k2->Left=K1->Right;
+    K1->Right=K2;
+    K2->Height=Max(Height(K2->Left),Height(K2->Right))+1;
+    K1->Height=Max(Height(K1->Left),K2->Height)+1;
+    return K1;
+}
+static Position SingleRotateWithRight(Position K2)
+{
+    Position K1;
+    K1=K2->Right;
+    K2->Right=K1->Left;
+    K1->Left=K2;
+    K2->Height=Max(Height(K2->Left),Height(K2->Right))+1;
+    K1->Height=Max(Height(K1->Right),K2->Height)+1;
+    return K1;
+}
+static Position DoubleRotateWithLeft(Position K3)
+{
+    K3->Left=SingleRotateWithRight(K3->Left);
+    return SingleRotateWithLeft(K3);
+}
+static Position DoubleRotateWithLeft(Position K3)
+{
+    K3->Right=SingleRotateWithLeft(K3->Right);
+    return SingleRotateWithRight(K3);
+}
+AvlTree Insert(int X,AvlTree T)
+{
+    if(T==NULL)
+    {
+    T=malloc(sizeof(struct AvlNode));
+    if(T==NULL)
+    FataError("Out of space!!!");
+    else
+    {
+        T->Data=X;
+        T->Height=0;
+        T->Left=T->Right=NULL;
+    }
+}
+else
+if(X<T->Data)
+{
+    T->Left=Insert(X,T->Left);
+    if(Height(T->Left)-Height(T->Right)==2)
+    if(X<T->Left->Data)
+    T=SingleRotateWithLeft(T);
+    else
+    T=DoubleRotateWithLeft(T);
+}
+else
+if(X>T->Data)
+{
+    T->Right=Insert(X,T->Right);
+    if(Height(T->Right)-Height(T->Left)==2)
+    if(X>T->Right->Data)
+    T=SingleRotateWithRight(T);
+    else
+    T=DoubleRotateWithRight(T);
+}
+T->Height=Max(Height(T->Left),Height(T->Right))+1;
+return T;
+}
