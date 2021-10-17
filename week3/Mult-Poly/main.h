4271b369ea299453c845c741d1361ca6e1cae9c4
wgennis <91460230+wgennis@users.noreply.github.com>
2021-10-17 19:29:04 +0800

    Create main.h

---
+++ week3/Mult-Poly/main.h
@@ -0,0 +1,11 @@
+#ifndef _HashSep_H
+struct ListNode;
+typedef struct ListNode* Position;
+struct HashTb1;
+typedef struct HashTbl* HashTable;
+HashTable InitializeTable(int TableSize);
+void DestroyTable(HashTable H);
+Position Find(int Key, HashTable H);
+void Insert(int Key1,int Key2, HashTable H);
+int Retrieve(Position P);
+#endif // !_HashSep_H
